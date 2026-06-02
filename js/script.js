252aaeca0f4dbe979dc8dd16a0986a0eb2bdb522
alxja9 <alxja09@icloud.com>
2026-06-02 14:42:14 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+let coins = 0;
 
+const coin = document.getElementById("cookie");
+const counter = document.getElementById("cookieCount");
+
+function updateDisplay() {
+    counter.textContent = coins + " Monedas";
+}
+
+coin.addEventListener("click", () => {
+    coins++;
+    updateDisplay();
+});
+
+updateDisplay();
